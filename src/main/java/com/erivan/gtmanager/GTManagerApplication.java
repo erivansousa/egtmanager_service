68d7a67568a64a898103fb4f99da958f56a83753
erivan sousa <erivan.sousa@dzsi.com>
2024-08-27 10:46:51 -0300

    initialize spring server side app project

---
+++ src/main/java/com/erivan/gtmanager/GTManagerApplication.java
@@ -0,0 +1,13 @@
+package com.erivan.gtmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GTManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GTManagerApplication.class, args);
+	}
+
+}
