ed6250cb262914cae5379325f68f7feb6fdc853c
erivan sousa <erivan.sousa@dzsi.com>
2024-09-06 16:00:26 -0300

    create unit tests for custom authentication filter

--- src/test/java/com/erivan/gtmanager/security/CustomAuthenticationFilterTest.java
+++ src/test/java/com/erivan/gtmanager/security/CustomAuthenticationFilterTest.java
@@ -1,39 +1,107 @@
 package com.erivan.gtmanager.security;
 
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.erivan.gtmanager.data.UserRepository;
+import com.erivan.gtmanager.data.entity.User;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 class CustomAuthenticationFilterTest {
 
-    CustomAuthenticationFilter filter;
-    JWTUtil jwtUtil;
-    UserRepository userRepository;
+    @Mock
+    private JWTUtil jwtUtil;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private CustomAuthenticationFilter customAuthenticationFilter;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private FilterChain filterChain;
 
     @BeforeEach
     void setUp() {
-        jwtUtil = Mockito.mock(JWTUtil.class);
-        userRepository = Mockito.mock(UserRepository.class);
-        filter = new CustomAuthenticationFilter(jwtUtil, userRepository);
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void when() {
-        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-        FilterChain filterChain = Mockito.mock(FilterChain.class);
+    void testDoFilterInternal_ValidToken() throws ServletException, IOException {
+        // Arrange
+        String token = "Bearer valid-token";
+        String userId = "user-id";
+        String userEmail = "user@example.com";
+        DecodedJWT decodedTokenMock = mock(DecodedJWT.class);
+        Claim uIdClaim = mock(Claim.class);
 
-        Mockito.when(request.getHeader("Authorization")).thenReturn("");
+        when(request.getHeader("Authorization")).thenReturn(token);
+        when(jwtUtil.decodeToken(token.substring(7))).thenReturn(decodedTokenMock);
+        when(uIdClaim.asString()).thenReturn(userId);
+        when(decodedTokenMock.getClaim("uid")).thenReturn(uIdClaim);
+        User user = new User();
+        user.setId(userId);
+        user.setEmail(userEmail);
+        user.setLastToken(new User.AccessToken(token, ""));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
 
-        try {
-            filter.doFilterInternal(request, response, filterChain);
-        } catch (Exception ignored) {
+        // Act
+        customAuthenticationFilter.doFilterInternal(request, response, filterChain);
 
-        }
+        // Assert
+        verify(filterChain, times(1)).doFilter(request, response);
+    }
+
+    @Test
+    void testDoFilterInternal_InvalidToken() throws ServletException, IOException {
+        // Arrange
+        String token = "Bearer invalid-token";
+
+        when(request.getHeader("Authorization")).thenReturn(token);
+        when(jwtUtil.decodeToken(token.substring(7))).thenReturn(null);
+
+        // Act
+        customAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        // Assert
+        verify(filterChain, times(1)).doFilter(request, response);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        assertNull(securityContext.getAuthentication());
+    }
+
+    @Test
+    void testDoFilterInternal_NoToken() throws ServletException, IOException {
+        // Arrange
+        when(request.getHeader("Authorization")).thenReturn(null);
+
+        // Act
+        customAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        // Assert
+        verify(filterChain, times(1)).doFilter(request, response);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        assertNull(securityContext.getAuthentication());
     }
 }
